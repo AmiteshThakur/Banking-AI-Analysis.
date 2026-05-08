bc60e15e1edcc335362428421ae9dd6978897473
Amitesh Thakur <118919571+AmiteshThakur@users.noreply.github.com>
2026-05-08 16:26:26 +0530

    frontend

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import AICompliancePlatform from './components/AICompliancePlatform'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <AICompliancePlatform/>
+    </>
+  )
+}
+
+export default App
